139cb5482976c69747c01c44a3378335a23fc33b
Josh <jolumese@live.com>
2020-09-23 17:51:23 -0400

    WTFBuzz
    Completed

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,7 +1,20 @@
 class WuTangForeverBuzz {
     wuTangClan(value){
         let output ="";
-
+        for (let i = 1; i <= value; i++) {
+        	if (i % 15 == 0) {
+        		output += 'WuTangForever\n';
+        		// console.log(output);
+        	} else if (i % 3 == 0) {
+        		output += 'Wu\n';
+        		// console.log(output);
+        	} else if (i % 5 == 0) {
+        		output += 'Tang\n';
+        		// console.log(output);
+        	} else {
+        		output += i.toString() + '\n';
+        	}
+        }
         return output;
     }
 }
